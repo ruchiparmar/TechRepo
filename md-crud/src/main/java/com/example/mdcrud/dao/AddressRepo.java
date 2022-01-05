5887c313eda1c598a839964d1b3719d655cb9d81
ruchiparmar <41139028+ruchiparmar@users.noreply.github.com>
2022-01-06 00:57:19 +0530

    Delete md-crud directory

--- md-crud/src/main/java/com/example/mdcrud/dao/AddressRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.mdcrud.dao;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.mdcrud.model.Address;
-
-public interface AddressRepo extends MongoRepository<Address, Long>
-{
-}
